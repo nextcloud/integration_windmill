19a80e137a1227fd4556fe67b8e272ec3ccddf9c
Jana Peper <jana.peper@nextcloud.com>
2025-09-19 15:17:12 +0200

    feat: event listing ocs endpoint
    Signed-off-by: Jana Peper <jana.peper@nextcloud.com>

--- lib/Controller/ApiController.php
+++ lib/Controller/ApiController.php
@@ -7,25 +7,76 @@ namespace OCA\Windmill\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\ApiRoute;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
+use OCP\AppFramework\Http\Attribute\NoCSRFRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\OCSController;
 
-/**
- * @psalm-suppress UnusedClass
- */
 class ApiController extends OCSController {
-	/**
-	 * An example API endpoint
+
+		/**
+	 * List all events that can be registered as a webhook
 	 *
-	 * @return DataResponse<Http::STATUS_OK, array{message: string}, array{}>
+	 * @return DataResponse<Http::STATUS_OK, array<string, mixed>, array{}>|DataResponse<Http::STATUS_NOT_FOUND, array{}, array{}>
 	 *
-	 * 200: Data returned
+	 * 200: event list
 	 */
+	#[ApiRoute(verb: 'GET', url: 'api/v1/list/events')]
 	#[NoAdminRequired]
-	#[ApiRoute(verb: 'GET', url: '/api')]
-	public function index(): DataResponse {
-		return new DataResponse(
-			['message' => 'Hello world!']
-		);
+	#[NoCSRFRequired]
+	public function listEvents(): DataResponse {
+
+	$events = [
+				[
+					"name" => "FormSubmittedEvent",
+					"description" => "A submission to a form in Nextcloud Forms",
+					"path" => "OCA\Forms\Events\FormSubmittedEvent",
+					"parameters" => [
+						"user" => ["uid" => "string", "displayName" => "string"],
+						"time" => "int",
+						"event" => [
+							"class" => "string",
+							"form" => [
+							"id" => "int",
+							"hash" => "string",
+							"title" => "string",
+							"description" => "string",
+							"ownerId" => "string",
+							"fileId" => "string|null",
+							"fileFormat" => "string|null",
+							"created" => "int",
+							"access" => "int",
+							"expires" => "int",
+							"isAnonymous" => "bool",
+							"submitMultiple"  => "bool",
+							"showExpiration" => "bool",
+							"lastUpdated"  => "int",
+							"submissionMessage" => "string|null",
+							"state" => "int",
+							],
+							"submission" => [
+							"id" => "int",
+							"formId" => "int",
+							"userId" => "string",
+							"timestamp" => "int",
+							],
+						]
+					],
+				],
+				[
+					"name" => "NodeWrittenEvent",
+					"description" => "A node in Nextcloud (a file/folder/similar) has been written",
+					"path" => "OCP\Files\Events\Node\NodeWrittenEvent",
+					"parameters" => [
+						"user" => ["uid" => "string", "displayName" => "string",
+						"time" => "int",
+						"event" => [
+							"class" => "string",
+							"node" => ["id" => "string", "path" => "string"]
+						]
+					],
+				],
+			]
+	];
+		return new DataResponse($events, Http::STATUS_OK);
 	}
 }
