066edb3154cbb6a6b17f8b3ac5b5c0ce4ab05126
Jana Peper <jana.peper@nextcloud.com>
2025-09-26 12:06:43 +0200

    test: fix lint
    Signed-off-by: Jana Peper <jana.peper@nextcloud.com>

--- src/main.js
+++ src/main.js
@@ -1,20 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { translate, translatePlural } from '@nextcloud/l10n'
 
 const app = createApp(App)
-	app.mixin({
-		methods: {
-			t: translate,
-			n: translatePlural,
-		},
-		computed: {
-			OCA() {
-				return window.OCA
-			},
-			OC() {
-				return window.OC
-			},
-		},
-	})
-	app.mount('#integration_windmill')
+app.mount('#integration_windmill')
